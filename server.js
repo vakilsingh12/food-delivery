4f74a30cd153dab64b7d7ae13ef3364d301e0bcf
Vakil <vakilverma7@yahoo.com>
2020-12-26 17:53:17 +0530

    UI updated

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const { Console } = require('console');
 const express=require('express');
 const app=express();
 const port=process.env.PORT||5000;
-// const expressLayout=require('express-ejs-layouts');
-// app.use(expressLayout);
 const path=require('path');
 const public_path=path.join(__dirname,'Public');
 app.use(express.static(public_path));
@@ -12,6 +10,15 @@ app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine',"ejs");
 app.get('/',(req,res)=>{
     res.render("Home");
+})
+app.get('/cart',(req,res)=>{
+    res.render("customers/cart");
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
 })
 app.listen(port,()=>{
     console.log(`server running on port ${port}`);
